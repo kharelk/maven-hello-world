5120d3aa5975519ed78b0eeb5c517009a360a80e
Harel Karavani <harel.karavani@sidelines.io>
2024-12-24 20:49:42 +0200

    update version in pom.xml and push back to repo after version change

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -1,13 +1,9 @@
 package com.myapp;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World!, from Harel" );
     }
 }
